0f8348cfbe8fd8f8fc89aadb111613b3ef16decc
TikhomirovRodion <tikhomirov.rodion@gmail.com>
2024-11-11 23:28:03 +0300

    Add files via upload

--- Resistors.py
+++ Resistors.py
@@ -8,19 +8,18 @@ def click_btn2():
 
 def click_btn3():
     print("function3")
-    
 tk = Tk()
 tk.title("Resistors")
 canvas = Canvas(tk, height=700, width=1000)
 
-canvas.create_text(10, 10, text='Выберите тип цепи', fill="#004D40")
+canvas.create_text(500, 50, text='Выберите тип цепи', fill="#004D40")
 
 btn1 = ttk.Button(text="Последовательное", command=click_btn1)
-btn1.pack()
+btn1.place(x=420, y=80)
 btn2 = ttk.Button(text="Параллельное", command=click_btn2)
-btn2.pack()
+btn2.place(x=435, y=105)
 btn3 = ttk.Button(text="Смешанное", command=click_btn3)
-btn3.pack()
+btn3.place(x=444, y=130)
 
 canvas.pack()
 
