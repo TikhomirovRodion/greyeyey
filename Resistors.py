86a49b679020ff0b0600132b46c2b3856486019f
TikhomirovRodion <tikhomirov.rodion@gmail.com>
2024-11-13 00:16:52 +0300

    Add files via upload
    Добавил схему последовательного соединения резисторов

--- Resistors.py
+++ Resistors.py
@@ -2,12 +2,49 @@ from tkinter import *
 from tkinter import ttk
 def click_btn1():
     print("function1")
+    canvas.delete('all')     #Очистка окна
+    btn3.destroy()
+    btn2.destroy()
+    btn1.destroy()
+
+    canvas.create_line(200, 100, 200, 300)      #Создание боковых проводов
+    canvas.create_line(802, 100, 802, 300)
+
+    n=0                                 #Создание цепочки резисторов
+    for i in range(0, 3):
+        n1 = 200+86*n
+        n2 = n1+86
+        n3 = n2+86
+        canvas.create_line(n1, 100, n2, 100)
+        canvas.create_rectangle(n2, 75, n3, 125)
+        n+=2
+    canvas.create_line(716, 100, 802, 100)
+
+    canvas.create_line(200, 300, 496, 300)      #Создание нижних проводов
+    canvas.create_line(506, 300, 802, 300)
+
+    canvas.create_line(496, 285, 496, 315)      #Создание источника питания
+    canvas.create_line(506, 275, 506, 325)
+
+    canvas.create_text(329, 100, text='R1', fill="#004D40")     #Создание обозначений резисторов
+    canvas.create_text(501, 100, text='R2', fill="#004D40")
+    canvas.create_text(673, 100, text='R3', fill="#004D40")
 
 def click_btn2():
     print("function2")
+    canvas.delete('all')
+    btn3.destroy()
+    btn2.destroy()
+    btn1.destroy()
 
 def click_btn3():
     print("function3")
+    canvas.delete('all')
+    btn3.destroy()
+    btn2.destroy()
+    btn1.destroy()
+
+
 tk = Tk()
 tk.title("Resistors")
 canvas = Canvas(tk, height=700, width=1000)
