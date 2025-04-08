b3b72823d27efd549a4ac7f245f13baa3fc08035
TikhomirovRodion <tikhomirov.rodion@gmail.com>
2025-04-08 20:06:19 +0300

    Add files via upload

--- Resistors.py
+++ Resistors.py
@@ -1,16 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 from random import randint
+
+tk = Tk()
+tk.title("Resistors")
+canvas = Canvas(tk, height=700, width=1000)
+
+
 def click_btn1():
     print("function1")
-    canvas.delete('all')     #Очистка окна
 
-    canvas.create_line(200, 100, 200, 300)      #Создание боковых проводов
-    canvas.create_line(802, 100, 802, 300)
+    tk1 = Tk()
+    tk1.title('Последовательное соединение')
+    canvas1 = Canvas(tk1, height=700, width=1000)
 
-    btn3.destroy()
-    btn2.destroy()
-    btn1.destroy()
+
+    canvas1.create_line(200, 100, 200, 300)      #Создание боковых проводов
+    canvas1.create_line(802, 100, 802, 300)
+
 
 
     n=0                                 #Создание цепочки резисторов
@@ -18,30 +25,30 @@ def click_btn1():
         n1 = 200+86*n
         n2 = n1+86
         n3 = n2+86
-        canvas.create_line(n1, 100, n2, 100)
-        canvas.create_rectangle(n2, 75, n3, 125)
+        canvas1.create_line(n1, 100, n2, 100)
+        canvas1.create_rectangle(n2, 75, n3, 125)
         n+=2
-    canvas.create_line(716, 100, 802, 100)
+    canvas1.create_line(716, 100, 802, 100)
 
-    canvas.create_line(200, 300, 496, 300)      #Создание нижних проводов
-    canvas.create_line(506, 300, 802, 300)
+    canvas1.create_line(200, 300, 496, 300)      #Создание нижних проводов
+    canvas1.create_line(506, 300, 802, 300)
 
-    canvas.create_line(400, 300, 400, 200)      #Создание вольтметра
-    canvas.create_line(600, 300, 600, 200)
-    canvas.create_line(400, 200, 470, 200)
-    canvas.create_line(530, 200, 600, 200)
-    canvas.create_text(500, 200, text='V')
+    canvas1.create_line(400, 300, 400, 200)      #Создание вольтметра
+    canvas1.create_line(600, 300, 600, 200)
+    canvas1.create_line(400, 200, 470, 200)
+    canvas1.create_line(530, 200, 600, 200)
+    canvas1.create_text(500, 200, text='V')
 
-    canvas.create_oval(470, 170, 530, 230)
+    canvas1.create_oval(470, 170, 530, 230)
 
-    canvas.create_line(496, 285, 496, 315)      #Создание источника питания
-    canvas.create_line(506, 275, 506, 325)
+    canvas1.create_line(496, 285, 496, 315)      #Создание источника питания
+    canvas1.create_line(506, 275, 506, 325)
 
-    canvas.create_text(329, 100, text='R1')     #Создание обозначений резисторов
-    canvas.create_text(501, 100, text='R2')
-    canvas.create_text(673, 100, text='R3')
+    canvas1.create_text(329, 100, text='R1')     #Создание обозначений резисторов
+    canvas1.create_text(501, 100, text='R2')
+    canvas1.create_text(673, 100, text='R3')
 
-    canvas.create_text(365, 430, text='Ответы запишите в формате: X.XX')
+    canvas1.create_text(365, 430, text='Ответы запишите в формате: X.XX')
 
     R1 = randint(1, 5)
     R2 = randint(1, 5)
@@ -55,11 +62,11 @@ def click_btn1():
     #print(round(I, 2))
 
     text = ("R1 = " + str(R1) + ' Ом;   R2 = ' + str(R2) + ' Ом;   R3 = ' + str(R3) + ' Ом;   Uv = ' + str(Uv) + ' B')
-    canvas.create_text(397, 405, text=text)
-    canvas.create_text(220, 465, text='U1')
-    canvas.create_text(220, 490, text='U2')
-    canvas.create_text(220, 515, text='U3')
-    canvas.create_text(220, 540, text='I')
+    canvas1.create_text(397, 405, text=text)
+    canvas1.create_text(220, 465, text='U1')
+    canvas1.create_text(220, 490, text='U2')
+    canvas1.create_text(220, 515, text='U3')
+    canvas1.create_text(220, 540, text='I')
 
 
     def click_btnAnsw():
@@ -67,30 +74,37 @@ def click_btn1():
         answU2 = entryU2.get()
         answU3 = entryU3.get()
         answI = entryI.get()
+
+
+        label2 = ttk.Label(text='')
+        label3 = ttk.Label(text='')
+        label4 = ttk.Label(text='')
         #print(answU1)
         I = Uv / (R1 + R2 + R3)
         U1 = I * R1
         U2 = I * R2
         U3 = I * R3
         if float(answU1) == round(U1, 2):
-            canvas.create_text(675, 465, text='Да')
+            label1 = ttk.Label(text='Да')
+            label1.place(x = 700, y = 450)
         else:
-            canvas.create_text(675, 465, text='Нет')
+            label1 = ttk.Label(text='Нет')
+            label1.place(x=700, y=450)
 
         if float(answU2) == round(U2, 2):
-            canvas.create_text(675, 490, text='Да')
+            canvas1.create_text(675, 490, text='Да')
         else:
-            canvas.create_text(675, 490, text='Нет')
+            canvas1.create_text(675, 490, text='Нет')
 
         if float(answU3) == round(U3, 2):
-            canvas.create_text(675, 515, text='Да')
+            canvas1.create_text(675, 515, text='Да')
         else:
-            canvas.create_text(675, 515, text='Нет')
+            canvas1.create_text(675, 515, text='Нет')
 
         if float(answI) == round(I, 2):
-            canvas.create_text(675, 540, text='Да')
+            canvas1.create_text(675, 540, text='Да')
         else:
-            canvas.create_text(675, 540, text='Нет')
+            canvas1.create_text(675, 540, text='Нет')
 
     btnAnsw = ttk.Button(text="Ответ", command=click_btnAnsw)
     btnAnsw.place(x=550, y=450)
@@ -214,9 +228,7 @@ def click_btn3():
 
 
 
-tk = Tk()
-tk.title("Resistors")
-canvas = Canvas(tk, height=700, width=1000)
+
 
 canvas.create_text(500, 50, text='Выберите тип цепи')
 
